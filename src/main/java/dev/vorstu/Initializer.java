7e5b282fcb13ca63b71251d8c272fa55b26344cc
Xblunt <andy080104@gmail.com>
2023-11-03 18:21:57 +0300

    Add more student

--- src/main/java/dev/vorstu/Initializer.java
+++ src/main/java/dev/vorstu/Initializer.java
@@ -16,5 +16,16 @@ public class Initializer {
         studentRepository.save(new Student("Masson", "g2", "+7988"));
         studentRepository.save(new Student("Kbup", "g2", "+7977"));
         studentRepository.save(new Student("Astak", "g3", "+7966"));
+        studentRepository.save(new Student("rrrr", "g4", "+7988"));
+        studentRepository.save(new Student("ttt", "g4", "+7933"));
+        studentRepository.save(new Student("yyy", "g5", "+7922"));
+        studentRepository.save(new Student("uuu", "g5", "+7911"));
+        studentRepository.save(new Student("iii", "g6", "+7977"));
+        studentRepository.save(new Student("ooo", "g7", "+7900"));
+        studentRepository.save(new Student("ppp", "g8", "+7966"));
+        studentRepository.save(new Student("fff", "g9", "+7944"));
+        studentRepository.save(new Student("bbb", "g10", "+7933"));
+
+
     }
 }
