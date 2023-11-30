46f266050862407cb8c0b2a98a258dff1b769485
Xblunt <andy080104@gmail.com>
2023-12-01 01:30:26 +0300

    oop3

--- src/main/java/dev/vorstu/controllers/HomeController.java
+++ src/main/java/dev/vorstu/controllers/HomeController.java
@@ -6,8 +6,8 @@ import dev.vorstu.db.entities.auth.BaseRole;
 import dev.vorstu.db.repositories.AuthUserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,18 +41,37 @@ public class HomeController {
 //                    .anyMatch(role -> !role.getRole().equals(BaseRole.SUPER_USER)))
 //            .map(el -> new ShortUser(el.getId(), el.getName(), el.getSurname()));
 //}
+//@GetMapping("/users")
+//public Page<ShortUser> getAllUsers(
+//        @RequestParam(defaultValue = "0") int page,
+//        @RequestParam(defaultValue = "2") int size) {
+//    PageRequest pageRequest = PageRequest.of(page, size);
+//    List<ShortUser> users = StreamSupport.stream(authUserRepo.findAll(pageRequest).spliterator(), false)
+//            .filter(el -> el.getRoles().stream()
+//
+//                    .anyMatch(role -> !role.getRole().equals(BaseRole.SUPER_USER)))
+//            .filter(el -> !el.getUsername().equals("admin"))
+//            .map(el -> new ShortUser(el.getRoleId(), el.getUsername(), el.getName(), el.getSurname()))
+//            .collect(Collectors.toList());
+//    return PageableExecutionUtils.getPage(users, pageRequest, () -> authUserRepo.count());
+//}
 @GetMapping("/users")
 public Page<ShortUser> getAllUsers(
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "2") int size) {
     PageRequest pageRequest = PageRequest.of(page, size);
     List<ShortUser> users = StreamSupport.stream(authUserRepo.findAll(pageRequest).spliterator(), false)
-            .filter(el -> el.getRoles().stream()
-
-                    .anyMatch(role -> !role.getRole().equals(BaseRole.SUPER_USER)))
-
+            .filter(el -> !el.getRoles().stream()
+                    .allMatch(role -> !role.getRole().equals(BaseRole.STUDENT)))
             .map(el -> new ShortUser(el.getRoleId(), el.getUsername(), el.getName(), el.getSurname()))
             .collect(Collectors.toList());
-    return PageableExecutionUtils.getPage(users, pageRequest, () -> authUserRepo.count());
+
+
+    return new PageImpl<>(users, pageRequest, authUserRepo.count());
+//    return new PageImpl<>(users, pageRequest, countStudents() );
 }
+
+//    private long countStudents() {
+//        return authUserRepo.countByRole(BaseRole.STUDENT);
+//    }
 }
--- src/main/java/dev/vorstu/db/repositories/AuthUserRepo.java
+++ src/main/java/dev/vorstu/db/repositories/AuthUserRepo.java
@@ -11,16 +11,13 @@ import java.util.Set;
 
 @Repository
 public interface AuthUserRepo extends PagingAndSortingRepository<AuthUserEntity, Long> {
-//    CrudRepository
-//@Query(value = "SELECT user_id, enabled, name, password, surname, username FROM users u" +
-//        " WHERE u.name LIKE :data OR u.surname LIKE :data",
-//        nativeQuery = true)
-//Page<AuthUserEntity> findWithFilter(Pageable pageable, @Param("data")String data);
+
    @Modifying
    @Query(value = "UPDATE users u SET u.enabled = false WHERE user_id = :id",
            nativeQuery = true)
    int deleteUser(@Param("id")Long user_id);
+;
    public Set<AuthUserEntity> findAll();
-//    int deleteUser(@Param("id")Long id);
+
 
 }
